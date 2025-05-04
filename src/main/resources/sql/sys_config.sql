4e863764a34fda2e38c950c914333b42474b6c52
ihrskyfish <ihr17867950748@126.com>
2025-05-04 11:14:29 +0000

    sql

--- src/main/resources/sql/sys_config.sql
+++ src/main/resources/sql/sys_config.sql
@@ -17,11 +17,24 @@ CREATE TABLE IF NOT EXISTS sys_config (
     del_flag TINYINT DEFAULT 0                  -- 删除标志（0：未删除，1：已删除）
 );
 
--- 插入一些示例数据（可选）
--- INSERT INTO sys_config (key_name, key_value, description, create_by, del_flag)
--- VALUES
--- ('example_key1', 'example_value1', 'This is an example configuration', 'admin', 0),
--- ('example_key2', 'example_value2', 'Another example configuration', 'admin', 0);
+
+
+INSERT INTO sys_config (key_name, key_value )
+VALUES
+('redis_event_queue_key', 'default_redis_event_queue_key'),
+('oss_access_key', 'your_oss_access_key'),
+('oss_access_secret', 'your_oss_access_secret'),
+('oss_bucket_name', 'your_oss_bucket_name'),
+('oss_endpoint', 'your_oss_endpoint'),
+('oss_endpoint_internal', 'your_oss_endpoint_internal'),
+('oss_upload_url', 'your_oss_upload_url'),
+('uploadpath', 'default_upload_path'),
+('plat_upload_path', 'default_plat_upload_path'),
+('plat_webapp_path', 'default_plat_webapp_path'),
+('plat_env', 'development'),
+('plat_interface_address', 'http://your_plat_interface_address'),
+('plat_name', 'default_platform_name'),
+('plat_upload_head_path', 'default_plat_upload_head_path');
 
 -- 验证表结构和数据
 SELECT * FROM sys_config;
